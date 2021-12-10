6a3a7f56f1f87449aa506f533c07ac560042e8d4
Gagandeep Singh <admin.gagan@gmail.com>
2021-12-10 12:11:52 +0530

    updated the tag

--- aws/first_demo/ec2.tf
+++ aws/first_demo/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "myawsserver" {
   instance_type = "t2.nano"
 
   tags = {
-    Name = "Gagandeep-aws-ec2-instance-v5"
+    Name = "Gagandeep-aws-ec2-instance-vodafone"
     Env = "test"
   }
 }
